9e8da5a80b59b169236f31ae977644f6b6ca4353
Adeline <deca.adeline@gmail.com>
2019-08-11 15:38:30 +0200

    fin chapitre sécurity : mise en place de l'authentification admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 use Doctrine\Common\Persistence\ObjectManager;
@@ -15,7 +15,9 @@ class SecurityController extends AbstractController
     * @Route("/login", name="login")
     */
     public function login(AuthenticationUtils $authenticationUtils){
+        // récupère les erreurs d'authentifications
         $error = $authenticationUtils->getLastAuthenticationError();
+        // permet de récupérer le dernier nom d'utilisateur tapé
         $lastUserName = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUserName,
